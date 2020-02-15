f5f9f8453132f6aeb4f1b5225a89deea0df87934
ofgulban <farukgulban@gmail.com>
2020-02-15 01:20:11 +0100

    Improve style

--- renzo_stat.h
+++ renzo_stat.h
@@ -1,20 +1,12 @@
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
-
-double ren_average(double arr[], int size) ; 
-
-double ren_stdev(double arr[], int size) ; 
-
-double ren_skew(double arr[], int size) ; 
-
-double ren_kurt(double arr[], int size) ; 
-
-double ren_autocor(double arr[], int size) ; 
-
-double ren_correl(double arr1[], double arr2[], int size) ; 
- 
+double ren_average(double arr[], int size);
+double ren_stdev(double arr[], int size);
+double ren_skew(double arr[], int size);
+double ren_kurt(double arr[], int size);
+double ren_autocor(double arr[], int size);
+double ren_correl(double arr1[], double arr2[], int size);
