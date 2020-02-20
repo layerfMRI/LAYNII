b4a20c5774e5da20fc664a7979b28c930c863370
ofgulban <farukgulban@gmail.com>
2020-02-20 20:47:21 +0100

    Removing after moveing them to laynii_lib

--- renzo_stat.h
+++
@@ -1,8 +0,0 @@
-
-// Declarations
-double ren_average(double arr[], int size);
-double ren_stdev(double arr[], int size);
-double ren_correl(double arr1[], double arr2[], int size);
-double ren_skew(double arr[], int size);
-double ren_kurt(double arr[], int size);
-double ren_autocor(double arr[], int size);
