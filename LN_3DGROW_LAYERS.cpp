3cf1beedc283481e10bbffffc3f7835dff43acf8
ofgulban <farukgulban@gmail.com>
2020-02-25 14:11:55 +0100

    WIP...

--- LN_3DGROW_LAYERS.cpp
+++ LN_3DGROW_LAYERS.cpp
@@ -87,21 +87,20 @@ int main(int argc, char*  argv[]) {
     // ========================================================================
     // Fix datatype issues
 
-    nifti_image* stepsfromWM0 = copy_nifti_header_as_float(nim_input);
-    float* stepsfromWM0_data = static_cast<float*>(stepsfromWM0->data);
-    nifti_image* distfromWM0 = copy_nifti_header_as_float(nim_input);
-    float* distfromWM0_data = static_cast<float*>(distfromWM0->data);
+    nifti_image* fromWM_steps = copy_nifti_header_as_float(nim_input);
+    float* fromWM_steps_data = static_cast<float*>(fromWM_steps->data);
+    nifti_image* fromWM_dist = copy_nifti_header_as_float(nim_input);
+    float* fromWM_dist_data = static_cast<float*>(fromWM_dist->data);
 
-    nifti_image* growfromWM1 = copy_nifti_header_as_float(nim_input);
-    float* growfromWM1_data = static_cast<float*>(growfromWM1->data);
+    nifti_image* fromGM_steps = copy_nifti_header_as_float(nim_input);
+    float* fromGM_steps_data = static_cast<float*>(fromGM_steps->data);
+    nifti_image* fromGM_dist = copy_nifti_header_as_float(nim_input);
+    float* fromGM_dist_data = static_cast<float*>(fromGM_dist->data);
 
-    nifti_image* stepsfromGM0 = copy_nifti_header_as_float(nim_input);
-    float* stepsfromGM0_data = static_cast<float*>(stepsfromGM0->data);
-    nifti_image* distfromGM0 = copy_nifti_header_as_float(nim_input);
-    float* distfromGM0_data = static_cast<float*>(distfromGM0->data);
-
-    nifti_image* growfromGM1 = copy_nifti_header_as_float(nim_input);
-    float* growfromGM1_data = static_cast<float*>(growfromGM1->data);
+    nifti_image* fromWM_id = copy_nifti_header_as_uint(nim_input);
+    unsigned int* fromWM_id_data = static_cast<unsigned int*>(fromWM_id->data);
+    nifti_image* fromGM_id = copy_nifti_header_as_uint(nim_input);
+    unsigned int* fromGM_id_data = static_cast<unsigned int*>(fromGM_id->data);
 
     nifti_image* WMkoordx1 = copy_nifti_header_as_int(nim_input);
     nifti_image* WMkoordy1 = copy_nifti_header_as_int(nim_input);
@@ -156,36 +155,37 @@ int main(int argc, char*  argv[]) {
     float dist_max = 0.;
 
     int nr_layers = 20;
+    int grow_vinc = 2;
 
-    cout << "  Start growing from WM..." << endl;
 
     // Setting zero
     for (int i = 0; i != nr_voxels; ++i) {
-        *(stepsfromWM0_data + i) = 0;
-        *(growfromWM1_data + i) = 0;
-        *(stepsfromGM0_data + i) = 0;
-        *(growfromGM1_data + i) = 0;
+        *(fromWM_steps_data + i) = 0;
+        *(fromWM_id_data + i) = 0;
+        *(fromGM_steps_data + i) = 0;
+        *(fromGM_id_data + i) = 0;
     }
 
     // ========================================================================
     // Grow from WM
     // ========================================================================
-    int grow_vinc = 2;
+    cout << "  Start growing from inner GM (WM-facing border)..." << endl;
 
     // Initialize grow volume
     for (int i = 0; i != nr_voxels; ++i) {
         if (*(nim_input_data + i) == 2) {  // WM boundary voxels within GM
-            *(stepsfromWM0_data + i) = 1.;
-            *(distfromWM0_data + i) = 1.;
+            *(fromWM_steps_data + i) = 1.;
+            *(fromWM_dist_data + i) = 1.;
+            *(fromWM_id_data + i) = i;
         } else {
-            *(stepsfromWM0_data + i) = 0.;
-            *(distfromWM0_data + i) = 0.;
+            *(fromWM_steps_data + i) = 0.;
+            *(fromWM_dist_data + i) = 0.;
         }
     }
 
     for (int grow_i = 1; grow_i != vinc; grow_i++) {
         for (int i = 0; i != nr_voxels; ++i) {
-            if (*(stepsfromWM0_data + i) == grow_i) {
+            if (*(fromWM_steps_data + i) == grow_i) {
                 int ix, iy, iz;
                 tie(ix, iy, iz) = ind2sub_3D(i, size_x, size_y);
 
@@ -195,100 +195,112 @@ int main(int argc, char*  argv[]) {
                 if (ix != 0) {
                     j = sub2ind_3D(ix-1, iy, iz, size_x, size_y);
                     if (*(nim_input_data + j) == 3) {
-                        d = *(distfromWM0_data + i) + dX;
-                        if (d < *(distfromWM0_data + j)
-                            || *(distfromWM0_data + j) == 0) {
-                            *(distfromWM0_data + j) = d;
-                            *(stepsfromWM0_data + j) = grow_i + 1;
+                        d = *(fromWM_dist_data + i) + dX;
+                        if (d < *(fromWM_dist_data + j)
+                            || *(fromWM_dist_data + j) == 0) {
+                            *(fromWM_dist_data + j) = d;
+                            *(fromWM_steps_data + j) = grow_i + 1;
+                            *(fromWM_id_data + j) = *(fromWM_id_data + i);
                         }
                     }
                 }
                 if (ix != size_x) {
                     j = sub2ind_3D(ix+1, iy, iz, size_x, size_y);
                     if (*(nim_input_data + j) == 3) {
-                        d = *(distfromWM0_data + i) + dX;
-                        if (d < *(distfromWM0_data + j)
-                            || *(distfromWM0_data + j) == 0) {
-                            *(distfromWM0_data + j) = d;
-                            *(stepsfromWM0_data + j) = grow_i + 1;
+                        d = *(fromWM_dist_data + i) + dX;
+                        if (d < *(fromWM_dist_data + j)
+                            || *(fromWM_dist_data + j) == 0) {
+                            *(fromWM_dist_data + j) = d;
+                            *(fromWM_steps_data + j) = grow_i + 1;
+                            *(fromWM_id_data + j) = *(fromWM_id_data + i);
                         }
                     }
                 }
                 if (iy != 0) {
                     j = sub2ind_3D(ix, iy-1, iz, size_x, size_y);
                     if (*(nim_input_data + j) == 3) {
-                        d = *(distfromWM0_data + i) + dY;
-                        if (d < *(distfromWM0_data + j)
-                            || *(distfromWM0_data + j) == 0) {
-                            *(distfromWM0_data + j) = d;
-                            *(stepsfromWM0_data + j) = grow_i + 1;
+                        d = *(fromWM_dist_data + i) + dY;
+                        if (d < *(fromWM_dist_data + j)
+                            || *(fromWM_dist_data + j) == 0) {
+                            *(fromWM_dist_data + j) = d;
+                            *(fromWM_steps_data + j) = grow_i + 1;
+                            *(fromWM_id_data + j) = *(fromWM_id_data + i);
                         }
                     }
                 }
                 if (iy != size_y) {
                     j = sub2ind_3D(ix, iy+1, iz, size_x, size_y);
                     if (*(nim_input_data + j) == 3) {
-                        d = *(distfromWM0_data + i) + dY;
-                        if (d < *(distfromWM0_data + j)
-                            || *(distfromWM0_data + j) == 0) {
-                            *(distfromWM0_data + j) = d;
-                            *(stepsfromWM0_data + j) = grow_i + 1;
+                        d = *(fromWM_dist_data + i) + dY;
+                        if (d < *(fromWM_dist_data + j)
+                            || *(fromWM_dist_data + j) == 0) {
+                            *(fromWM_dist_data + j) = d;
+                            *(fromWM_steps_data + j) = grow_i + 1;
+                            *(fromWM_id_data + j) = *(fromWM_id_data + i);
                         }
                     }
                 }
                 if (iz != 0) {
                     j = sub2ind_3D(ix, iy, iz-1, size_x, size_y);
                     if (*(nim_input_data + j) == 3) {
-                        d = *(distfromWM0_data + i) + dZ;
-                        if (d < *(distfromWM0_data + j)
-                            || *(distfromWM0_data + j) == 0) {
-                            *(distfromWM0_data + j) = d;
-                            *(stepsfromWM0_data + j) = grow_i + 1;
+                        d = *(fromWM_dist_data + i) + dZ;
+                        if (d < *(fromWM_dist_data + j)
+                            || *(fromWM_dist_data + j) == 0) {
+                            *(fromWM_dist_data + j) = d;
+                            *(fromWM_steps_data + j) = grow_i + 1;
+                            *(fromWM_id_data + j) = *(fromWM_id_data + i);
                         }
                     }
                 }
                 if (iz != size_z) {
                     j = sub2ind_3D(ix, iy, iz+1, size_x, size_y);
                     if (*(nim_input_data + j) == 3) {
-                        d = *(distfromWM0_data + i) + dZ;
-                        if (d < *(distfromWM0_data + j)
-                            || *(distfromWM0_data + j) == 0) {
-                            *(distfromWM0_data + j) = d;
-                            *(stepsfromWM0_data + j) = grow_i + 1;
+                        d = *(fromWM_dist_data + i) + dZ;
+                        if (d < *(fromWM_dist_data + j)
+                            || *(fromWM_dist_data + j) == 0) {
+                            *(fromWM_dist_data + j) = d;
+                            *(fromWM_steps_data + j) = grow_i + 1;
+                            *(fromWM_id_data + j) = *(fromWM_id_data + i);
                         }
                     }
                 }
             }
         }
     }
-    if (nifti_set_filenames(stepsfromWM0, "stepsfromWM0.nii", 1, 1)) {
+    if (nifti_set_filenames(fromWM_steps, "fromWM_steps.nii", 1, 1)) {
         return 1;
     }
-    nifti_image_write(stepsfromWM0);
+    nifti_image_write(fromWM_steps);
 
-    if (nifti_set_filenames(distfromWM0, "distfromWM0.nii", 1, 1)) {
+    if (nifti_set_filenames(fromWM_dist, "fromWM_dist.nii", 1, 1)) {
         return 1;
     }
-    nifti_image_write(distfromWM0);
+    nifti_image_write(fromWM_dist);
+
+    if (nifti_set_filenames(fromWM_id, "fromWM_id.nii", 1, 1)) {
+        return 1;
+    }
+    nifti_image_write(fromWM_id);
 
     // ========================================================================
     // Grow from CSF
     // ========================================================================
-    cout << "  Start growing from CSF ..." << endl;
+    cout << "  Start growing from outer GM..." << endl;
 
     for (int i = 0; i != nr_voxels; ++i) {
         if (*(nim_input_data + i) == 1) {
-            *(stepsfromGM0_data + i) = 1.;
-            *(distfromGM0_data + i) = 1.;
+            *(fromGM_steps_data + i) = 1.;
+            *(fromGM_dist_data + i) = 1.;
+            *(fromGM_id_data + i) = i;
         } else {
-            *(stepsfromGM0_data + i) = 0.;
-            *(distfromGM0_data + i) = 0.;
+            *(fromGM_steps_data + i) = 0.;
+            *(fromGM_dist_data + i) = 0.;
         }
     }
 
     for (int grow_i = 1; grow_i != vinc; grow_i++) {
         for (int i = 0; i != nr_voxels; ++i) {
-            if (*(stepsfromGM0_data + i) == grow_i) {
+            if (*(fromGM_steps_data + i) == grow_i) {
                 int ix, iy, iz;
                 tie(ix, iy, iz) = ind2sub_3D(i, size_x, size_y);
 
@@ -298,81 +310,92 @@ int main(int argc, char*  argv[]) {
                 if (ix != 0) {
                     j = sub2ind_3D(ix-1, iy, iz, size_x, size_y);
                     if (*(nim_input_data + j) == 3) {
-                        d = *(distfromGM0_data + i) + dX;
-                        if (d < *(distfromGM0_data + j)
-                            || *(distfromGM0_data + j) == 0) {
-                            *(distfromGM0_data + j) = d;
-                            *(stepsfromGM0_data + j) = grow_i + 1;
+                        d = *(fromGM_dist_data + i) + dX;
+                        if (d < *(fromGM_dist_data + j)
+                            || *(fromGM_dist_data + j) == 0) {
+                            *(fromGM_dist_data + j) = d;
+                            *(fromGM_steps_data + j) = grow_i + 1;
+                            *(fromGM_id_data + j) = *(fromGM_id_data + i);
                         }
                     }
                 }
                 if (ix != size_x) {
                     j = sub2ind_3D(ix+1, iy, iz, size_x, size_y);
                     if (*(nim_input_data + j) == 3) {
-                        d = *(distfromGM0_data + i) + dX;
-                        if (d < *(distfromGM0_data + j)
-                            || *(distfromGM0_data + j) == 0) {
-                            *(distfromGM0_data + j) = d;
-                            *(stepsfromGM0_data + j) = grow_i + 1;
+                        d = *(fromGM_dist_data + i) + dX;
+                        if (d < *(fromGM_dist_data + j)
+                            || *(fromGM_dist_data + j) == 0) {
+                            *(fromGM_dist_data + j) = d;
+                            *(fromGM_steps_data + j) = grow_i + 1;
+                            *(fromGM_id_data + j) = *(fromGM_id_data + i);
                         }
                     }
                 }
                 if (iy != 0) {
                     j = sub2ind_3D(ix, iy-1, iz, size_x, size_y);
                     if (*(nim_input_data + j) == 3) {
-                        d = *(distfromGM0_data + i) + dY;
-                        if (d < *(distfromGM0_data + j)
-                            || *(distfromGM0_data + j) == 0) {
-                            *(distfromGM0_data + j) = d;
-                            *(stepsfromGM0_data + j) = grow_i + 1;
+                        d = *(fromGM_dist_data + i) + dY;
+                        if (d < *(fromGM_dist_data + j)
+                            || *(fromGM_dist_data + j) == 0) {
+                            *(fromGM_dist_data + j) = d;
+                            *(fromGM_steps_data + j) = grow_i + 1;
+                            *(fromGM_id_data + j) = *(fromGM_id_data + i);
                         }
                     }
                 }
                 if (iy != size_y) {
                     j = sub2ind_3D(ix, iy+1, iz, size_x, size_y);
                     if (*(nim_input_data + j) == 3) {
-                        d = *(distfromGM0_data + i) + dY;
-                        if (d < *(distfromGM0_data + j)
-                            || *(distfromGM0_data + j) == 0) {
-                            *(distfromGM0_data + j) = d;
-                            *(stepsfromGM0_data + j) = grow_i + 1;
+                        d = *(fromGM_dist_data + i) + dY;
+                        if (d < *(fromGM_dist_data + j)
+                            || *(fromGM_dist_data + j) == 0) {
+                            *(fromGM_dist_data + j) = d;
+                            *(fromGM_steps_data + j) = grow_i + 1;
+                            *(fromGM_id_data + j) = *(fromGM_id_data + i);
                         }
                     }
                 }
                 if (iz != 0) {
                     j = sub2ind_3D(ix, iy, iz-1, size_x, size_y);
                     if (*(nim_input_data + j) == 3) {
-                        d = *(distfromGM0_data + i) + dZ;
-                        if (d < *(distfromGM0_data + j)
-                            || *(distfromGM0_data + j) == 0) {
-                            *(distfromGM0_data + j) = d;
-                            *(stepsfromGM0_data + j) = grow_i + 1;
+                        d = *(fromGM_dist_data + i) + dZ;
+                        if (d < *(fromGM_dist_data + j)
+                            || *(fromGM_dist_data + j) == 0) {
+                            *(fromGM_dist_data + j) = d;
+                            *(fromGM_steps_data + j) = grow_i + 1;
+                            *(fromGM_id_data + j) = *(fromGM_id_data + i);
                         }
                     }
                 }
                 if (iz != size_z) {
                     j = sub2ind_3D(ix, iy, iz+1, size_x, size_y);
                     if (*(nim_input_data + j) == 3) {
-                        d = *(distfromGM0_data + i) + dZ;
-                        if (d < *(distfromGM0_data + j)
-                            || *(distfromGM0_data + j) == 0) {
-                            *(distfromGM0_data + j) = d;
-                            *(stepsfromGM0_data + j) = grow_i + 1;
+                        d = *(fromGM_dist_data + i) + dZ;
+                        if (d < *(fromGM_dist_data + j)
+                            || *(fromGM_dist_data + j) == 0) {
+                            *(fromGM_dist_data + j) = d;
+                            *(fromGM_steps_data + j) = grow_i + 1;
+                            *(fromGM_id_data + j) = *(fromGM_id_data + i);
                         }
                     }
                 }
             }
         }
     }
-    if (nifti_set_filenames(stepsfromGM0, "stepsfromGM0.nii", 1, 1)) {
+    if (nifti_set_filenames(fromGM_steps, "fromGM_steps.nii", 1, 1)) {
         return 1;
     }
-    nifti_image_write(stepsfromGM0);
+    nifti_image_write(fromGM_steps);
 
-    if (nifti_set_filenames(distfromGM0, "distfromGM0.nii", 1, 1)) {
+    if (nifti_set_filenames(fromGM_dist, "fromGM_dist.nii", 1, 1)) {
         return 1;
     }
-    nifti_image_write(distfromGM0);
+    nifti_image_write(fromGM_dist);
+
+    if (nifti_set_filenames(fromGM_id, "fromGM_id.nii", 1, 1)) {
+        return 1;
+    }
+    nifti_image_write(fromGM_id);
     // ========================================================================
     // ========================================================================
     // ========================================================================
@@ -389,26 +412,32 @@ int main(int argc, char*  argv[]) {
 
     for (int i = 0; i != nr_voxels; ++i) {
         if (*(nim_input_data + i) == 3) {
-            GMK2_i = *(GMkoordx2_data + i);
-            GMK3_i = *(GMkoordy2_data + i);
+            GMK2_i  = *(GMkoordx2_data + i);
+            GMK3_i  = *(GMkoordy2_data + i);
             GMKz2_i = *(GMkoordz2_data+ i);
 
-            WMK2_i = *(WMkoordx2_data + i);
-            WMK3_i = *(WMkoordy2_data + i);
+            WMK2_i  = *(WMkoordx2_data + i);
+            WMK3_i  = *(WMkoordy2_data + i);
             WMKz2_i = *(WMkoordz2_data + i);
 
-            GMK2_f = static_cast<float>(GMK2_i);
-            GMK3_f = static_cast<float>(GMK3_i);
+            GMK2_f  = static_cast<float>(GMK2_i);
+            GMK3_f  = static_cast<float>(GMK3_i);
             GMKz2_f = static_cast<float>(GMKz2_i);
 
-            WMK2_f = static_cast<float>(WMK2_i);
-            WMK3_f = static_cast<float>(WMK3_i);
+            WMK2_f  = static_cast<float>(WMK2_i);
+            WMK3_f  = static_cast<float>(WMK3_i);
             WMKz2_f = static_cast<float>(WMKz2_i);
 
             int ix, iy, iz;
             tie(ix, iy, iz) = ind2sub_3D(i, size_x, size_y);
 
-            *(equi_dist_layers_data + i) = 19 * (1 - dist((float)ix, (float)iy, (float)iz, GMK2_f, GMK3_f, GMKz2_f, dX, dY, dZ) / (dist((float)ix, (float)iy, (float)iz, GMK2_f, GMK3_f, GMKz2_f, dX, dY, dZ) + dist((float)ix, (float)iy, (float)iz, WMK2_f, WMK3_f, WMKz2_f, dX, dY, dZ))) + 2;
+
+            float dist1, dist2, dist3;
+            dist1 = dist((float)ix, (float)iy, (float)iz, GMK2_f, GMK3_f, GMKz2_f, dX, dY, dZ);
+            dist2 = dist((float)ix, (float)iy, (float)iz, GMK2_f, GMK3_f, GMKz2_f, dX, dY, dZ);
+            dist3 = dist((float)ix, (float)iy, (float)iz, WMK2_f, WMK3_f, WMKz2_f, dX, dY, dZ);
+
+            *(equi_dist_layers_data + i) = 19 * (1 - dist1 / (dist2 + dist3)) + 2;
         }
     }
     cout << "  Running until stage 4..." << endl;
