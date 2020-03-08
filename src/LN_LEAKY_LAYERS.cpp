50bc2882ef8c1adcb3b5cc9fba59810dbcbf17ff
ofgulban <farukgulban@gmail.com>
2020-03-08 13:20:09 +0100

    Tidy up

--- src/LN_LEAKY_LAYERS.cpp
+++ src/LN_LEAKY_LAYERS.cpp
@@ -22,7 +22,7 @@ int show_help(void) {
     "    - This program now supports INT16, INT32 and FLOAT32. For other \n"
     "      data types use: \n"
     "          3dcalc -a rim.nii -datum short -expr 'a' -prefix rim_short.nii\n"
-    "    - This can be 3D. Hence the rim file should be dmsmooth in all three\n"
+    "    - This can be 3D. Hence the rim file should be dmnii_smooth in all three\n"
     "      dimensions.\n"
     "\n");
     return 0;
@@ -74,260 +74,147 @@ int main(int argc, char * argv[]) {
     log_nifti_descriptives(nim_inputr);
 
     // Get dimensions of input
-    int sizeSlice = nim_inputr->nz;
-    int sizePhase = nim_inputr->nx;
-    int sizeRead = nim_inputr->ny;
-    int nrep = nim_inputr->nt;
+    int size_x = nim_inputr->nx;
+    int size_y = nim_inputr->ny;
+    int size_z = nim_inputr->nz;
+    int size_t = nim_inputr->nt;
+    int nr_voxels = nim_inputr->nvox;
     int nx = nim_inputr->nx;
     int nxy = nim_inputr->nx * nim_inputr->ny;
     int nxyz = nim_inputr->nx * nim_inputr->ny * nim_inputr->nz;
     float dX = nim_inputr->pixdim[1];
     float dY = nim_inputr->pixdim[2];
     float dZ = nim_inputr->pixdim[3];
-
-    nifti_image * nim_input = nifti_copy_nim_info(nim_inputr);
-    nim_input->datatype = NIFTI_TYPE_FLOAT32;
-    nim_input->nbyper = sizeof(float);
-    nim_input->data = calloc(nim_input->nvox, nim_input->nbyper);
-    float * nim_input_data = (float *) nim_input->data;
-
-    //////////////////////////////////////////////////////////////
-    // Fixing potential problems with different input datatypes //
-    //////////////////////////////////////////////////////////////
-    if (nim_inputr->datatype == NIFTI_TYPE_FLOAT32) {
-        float * nim_inputr_data = (float *) nim_inputr->data;
-        for (int it = 0; it < nrep; ++it) {
-            for (int islice = 0; islice < sizeSlice; ++islice) {
-                for (int iy = 0; iy < sizePhase; ++iy) {
-                    for (int ix = 0; ix < sizeRead; ++ix) {
-                        *(nim_input_data + nxyz * it + nxy * islice + nx * ix + iy) = (float) (*(nim_inputr_data + nxyz * it + nxy * islice + nx * ix + iy));
-                        if ((*(nim_inputr_data + nxyz * it + nxy * islice + nx * ix + iy)) != 0  && (*(nim_inputr_data + nxyz * it + nxy * islice + nx * ix + iy)) <= 4) {
-                            *(nim_input_data + nxyz * it + nxy * islice + nx * ix + iy) = (float) (*(nim_inputr_data + nxyz * it + nxy * islice + nx * ix + iy));
-                        }
-                        else *(nim_input_data + nxyz * it + nxy * islice + nx * ix + iy) = 0;
-                    }
-                }
-            }
-        }
-    }
-
-    if (nim_inputr->datatype == NIFTI_TYPE_INT16) {
-        short *nim_inputr_data = (short *) nim_inputr->data;
-        for (int it = 0; it < nrep; ++it) {
-            for (int islice = 0; islice < sizeSlice; ++islice) {
-                for (int iy = 0; iy < sizePhase; ++iy) {
-                    for (int ix = 0; ix < sizeRead; ++ix) {
-                        *(nim_input_data + nxyz * it + nxy * islice + nx * ix + iy) = (float) (*(nim_inputr_data + nxyz * it + nxy * islice + nx * ix + iy));
-                    }
-                }
-            }
-        }
-    }
-
-    if (nim_inputr->datatype == NIFTI_TYPE_INT32) {
-        int *nim_inputr_data = (int *) nim_inputr->data;
-        for (int it = 0; it < nrep; ++it) {
-            for (int islice = 0; islice < sizeSlice; ++islice) {
-                for (int iy = 0; iy < sizePhase; ++iy) {
-                    for (int ix = 0; ix < sizeRead; ++ix) {
-                        *(nim_input_data + nxyz * it + nxy * islice + nx * ix + iy) = (float) (*(nim_inputr_data + nxyz * it + nxy * islice + nx * ix + iy));
-                    }
-                }
-            }
-        }
-    }
-    // cout << "  nim_input->intent_code " << nim_input->intent_code << endl;
-
-    ////////////////////////////
-    // Make it 2D if you want //
-    ////////////////////////////
     if (dim == 2) {
         dZ = 1000.;
         cout << "  Calculating layers only in 2D." << endl;
     }
 
-    nifti_image *smoothed = nifti_copy_nim_info(nim_input);
-    nifti_image *gausweight = nifti_copy_nim_info(nim_input);
-    nifti_image *layer = nifti_copy_nim_info(nim_input);
-    nifti_image *leak_layer = nifti_copy_nim_info(nim_input);
+    // ========================================================================
+    // Fixing potential problems with different input datatypes
+    nifti_image* nim_input = copy_nifti_as_float32(nim_inputr);
+    float* nim_input_data = static_cast<float*>(nim_input->data);
 
-    smoothed->datatype = NIFTI_TYPE_FLOAT32;
-    gausweight->datatype = NIFTI_TYPE_FLOAT32;
-    layer->datatype = NIFTI_TYPE_FLOAT32;
-    leak_layer->datatype = NIFTI_TYPE_FLOAT32;
+    // Allocate new nifti images
+    nifti_image* nii_smooth = copy_nifti_as_float32(nim_input);
+    float* nii_smooth_data = static_cast<float*>(nii_smooth->data);
+    nifti_image* nii_gaussw = copy_nifti_as_float32(nim_input);
+    float* nii_gaussw_data = static_cast<float*>(nii_gaussw->data);
+    nifti_image* nii_layer = copy_nifti_as_float32(nim_input);
+    float* nii_layer_data = static_cast<float*>(nii_layer->data);
+    nifti_image* nii_leaky = copy_nifti_as_float32(nim_input);
+    float* nii_leaky_data = static_cast<float*>(nii_leaky->data);
 
-    smoothed->nbyper = sizeof(float);
-    gausweight->nbyper = sizeof(float);
-    layer->nbyper = sizeof(float);
-    leak_layer->nbyper = sizeof(float);
-
-    smoothed->data = calloc(smoothed->nvox, smoothed->nbyper);
-    gausweight->data = calloc(gausweight->nvox, gausweight->nbyper);
-    layer->data = calloc(layer->nvox, layer->nbyper);
-    leak_layer->data = calloc(leak_layer->nvox, leak_layer->nbyper);
-
-    float *smoothed_data = (float *) smoothed->data;
-    float *gausweight_data = (float *) gausweight->data;
-    float *layer_data = (float *) layer->data;
-    float *leak_layer_data = (float *) leak_layer->data;
-
-    float dist(float x1, float y1, float z1, float x2, float y2, float z2,
-               float dX, float dY, float dZ);
-    float gaus(float distance, float sigma);
-
+    // ========================================================================
     cout << "  Debug 2 " << endl;
     float kernel_size = 2;  // Corresponds to one voxel sice.
-    int vinc = max(1., 2. * kernel_size);  // Ignore If voxel is too far
+    int vic = max(1., 2. * kernel_size);  // Ignore If voxel is too far
     float dist_i = 0.;
-    cout << "  vinc " << vinc << endl;
+    cout << "  vic " << vic << endl;
     cout << "  kernel_size " << kernel_size << endl;
 
-    int number_of_layers = 20;
-    for (int iz = 0; iz < sizeSlice; ++iz) {
-        for (int iy = 0; iy < sizePhase; ++iy) {
-            for (int ix = 0; ix < sizeRead; ++ix) {
-                if (*(nim_input_data + nxy * iz + nx * ix + iy) == 1) {
-                     *(layer_data + nxy * iz + nx * ix + iy) = -200.;
-                }
-                if (*(nim_input_data + nxy * iz + nx * ix + iy) == 2) {
-                    *(layer_data + nxy * iz + nx * ix + iy) = 200.;
-                }
-                if (*(nim_input_data + nxy * iz + nx * ix + iy) == 3) {
-                    *(layer_data + nxy * iz + nx * ix + iy) = 0.;
-                }
-            }
+    for (int i = 0; i < nr_voxels; ++i) {
+        if (*(nim_input_data + i) == 1) {
+            *(nii_layer_data + i) = -200.;
+        }
+        if (*(nim_input_data + i) == 2) {
+            *(nii_layer_data + i) = 200.;
+        }
+        if (*(nim_input_data + i) == 3) {
+            *(nii_layer_data + i) = 0.;
         }
     }
 
     ///////////////////////////////
     // Start iterative loop here //
     ///////////////////////////////
-    int N_iteratiosn = 400;
+    int iter_max = 400;
 
-    float dummy = 0;
-    for (int iteration = 0; iteration < N_iteratiosn; ++iteration) {
-        cout << "\r  Iteration: " << iteration << " of " << N_iteratiosn << flush;
-        // cout << "  Debug 3 = " << gaus(1 , 3) << endl;
-        for (int iz = 0; iz < sizeSlice; ++iz) {
-            for (int iy = 0; iy < sizePhase; ++iy) {
-                for (int ix = 0; ix < sizeRead-0; ++ix) {
-                    *(gausweight_data + nxy * iz + nx * ix + iy) = 0;
-                    *(smoothed_data + nxy * iz + nx * ix + iy) = 0;
-                    if (*(nim_input_data + nxy * iz + nx * ix + iy)  > 0) {
-                        for (int iz_i = max(0, iz - vinc); iz_i < min(iz + vinc + 1, sizeRead); ++iz_i) {
-                            for (int iy_i = max(0, iy - vinc); iy_i < min(iy + vinc + 1, sizePhase); ++iy_i) {
-                                for (int ix_i = max(0, ix - vinc); ix_i < min(ix + vinc + 1, sizeRead); ++ix_i) {
-                                    if (*(nim_input_data + nxy * iz_i + nx * ix_i + iy_i)  > 0) {
-                                        dist_i = dist((float)ix, (float)iy, (float)iz, (float)ix_i, (float)iy_i, (float)iz_i, dX, dY, dZ);
-                                        // cout << "debug  4 " << gaus(dist_i , kernel_size)  endl;
-                                        // if (*(nim_input_data + nxy * iz + nx * ix + iy) == 3) cout << "  Debug 4b " << endl;
-                                        if (dist_i < vinc && *(nim_input_data + nxy * iz + nx * ix + iy) == 3) {
-                                            //dummy = * (layer_data + nxy * iz_i + nx * ix_i + iy_i);
-                                            *(smoothed_data + nxy * iz + nx * ix + iy) = *(smoothed_data + nxy * iz + nx * ix + iy) + *(layer_data + nxy * iz_i + nx * ix_i + iy_i) * gaus(dist_i, kernel_size);
-                                            *(gausweight_data + nxy * iz + nx * ix + iy) = *(gausweight_data + nxy * iz + nx * ix + iy) + gaus(dist_i, kernel_size);
+    for (int iter = 0; iter < iter_max; ++iter) {
+        cout << "\r  Iteration: " << iter << " of " << iter_max << flush;
+        for (int iz = 0; iz < size_z; ++iz) {
+            for (int iy = 0; iy < size_x; ++iy) {
+                for (int ix = 0; ix < size_y-0; ++ix) {
+                    int voxel_i = nxy * iz + nx * ix + iy;
+                    *(nii_gaussw_data + voxel_i) = 0;
+                    *(nii_smooth_data + voxel_i) = 0;
+                    if (*(nim_input_data + voxel_i)  > 0) {
+                        for (int iz_i = max(0, iz - vic);
+                             iz_i < min(iz + vic + 1, size_y); ++iz_i) {
+                            for (int iy_i = max(0, iy - vic);
+                                 iy_i < min(iy + vic + 1, size_x); ++iy_i) {
+                                for (int ix_i = max(0, ix - vic);
+                                     ix_i < min(ix + vic + 1, size_y); ++ix_i) {
+                                    int voxel_j = nxy * iz_i + nx * ix_i + iy_i;
+                                    if (*(nim_input_data + voxel_j)  > 0) {
+                                        dist_i = dist((float)ix, (float)iy, (float)iz,
+                                                      (float)ix_i, (float)iy_i, (float)iz_i,
+                                                      dX, dY, dZ);
+                                        if (dist_i < vic
+                                            && *(nim_input_data + voxel_i) == 3) {
+                                            *(nii_smooth_data + voxel_i) += *(nii_layer_data + voxel_j) * gaus(dist_i, kernel_size);
+                                            *(nii_gaussw_data + voxel_i) += gaus(dist_i, kernel_size);
                                         }
                                     }
                                 }
                             }
                         }
-                        if (*(gausweight_data + nxy * iz + nx * ix + iy) >= 0) {
-                            *(smoothed_data + nxy * iz + nx * ix + iy) = *(smoothed_data + nxy * iz + nx * ix + iy)/ *(gausweight_data + nxy * iz + nx * ix + iy);
+                        if (*(nii_gaussw_data + voxel_i) >= 0) {
+                            *(nii_smooth_data + voxel_i) /= *(nii_gaussw_data + voxel_i);
                         }
                     }
                 }
             }
         }
-        for (int iz = 0; iz < sizeSlice; ++iz) {
-            for (int iy = 0; iy < sizePhase; ++iy) {
-                for (int ix = 0; ix < sizeRead; ++ix) {
-                    if (*(nim_input_data + nxy * iz + nx * ix + iy) == 1) {
-                        *(layer_data + nxy * iz + nx * ix + iy) = -200;
+        for (int iz = 0; iz < size_z; ++iz) {
+            for (int iy = 0; iy < size_x; ++iy) {
+                for (int ix = 0; ix < size_y; ++ix) {
+                    int voxel_i = nxy * iz + nx * ix + iy;
+                    if (*(nim_input_data + voxel_i) == 1) {
+                        *(nii_layer_data + voxel_i) = -200;
                     }
-                    if (*(nim_input_data + nxy * iz + nx * ix + iy) == 2) {
-                        *(layer_data + nxy * iz + nx * ix + iy) = 200;
+                    if (*(nim_input_data + voxel_i) == 2) {
+                        *(nii_layer_data + voxel_i) = 200;
                     }
-                    if (*(nim_input_data + nxy * iz + nx * ix + iy) == 3) {
-                        *(layer_data + nxy * iz + nx * ix + iy) = *(smoothed_data + nxy * iz + nx * ix + iy);
+                    if (*(nim_input_data + voxel_i) == 3) {
+                        *(nii_layer_data + voxel_i) = *(nii_smooth_data + voxel_i);
                     }
                 }
             }
         }
     }  // Iteration loop closed
 
-    for (int iz = 0; iz < sizeSlice; ++iz) {
-        for (int iy = 0; iy < sizePhase; ++iy) {
-            for (int ix = 0; ix < sizeRead; ++ix) {
-                *(leak_layer_data + nxy * iz + nx * ix + iy) = 2+19 * (*(smoothed_data + nxy * iz + nx * ix + iy) -(-200))/(200-(-200));
-                if (*(nim_input_data + nxy * iz + nx * ix + iy) == 1) {
-                    *(leak_layer_data + nxy * iz + nx * ix + iy) = 1;
-                }
-                if (*(nim_input_data + nxy * iz + nx * ix + iy) == 2) {
-                    *(leak_layer_data + nxy * iz + nx * ix + iy) = 21;
-                }
-                if (*(nim_input_data + nxy * iz + nx * ix + iy) == 0) {
-                    *(leak_layer_data + nxy * iz + nx * ix + iy) = 0;
-                }
-                if (*(leak_layer_data + nxy * iz + nx * ix + iy) < 0) {
-                    *(leak_layer_data + nxy * iz + nx * ix + iy) = 0;
-                }
-            }
+
+    for (int i = 0; i < nr_voxels; ++i) {
+        *(nii_leaky_data + i) = 2 + 19 * (*(nii_smooth_data + i) - (-200))
+                                / (200 - (-200));
+        if (*(nim_input_data + i) == 1) {
+            *(nii_leaky_data + i) = 1;
+        }
+        if (*(nim_input_data + i) == 2) {
+            *(nii_leaky_data + i) = 21;
+        }
+        if (*(nim_input_data + i) == 0) {
+            *(nii_leaky_data + i) = 0;
+        }
+        if (*(nii_leaky_data + i) < 0) {
+            *(nii_leaky_data + i) = 0;
         }
     }
 
-    for (int iz = 0; iz < sizeSlice; ++iz) {
-        for (int iy = 0; iy < sizePhase; ++iy) {
-            for (int ix = 0; ix < sizeRead; ++ix) {
-                if (*(nim_input_data + nxy * iz + nx * ix + iy) != 0) {
-                    *(leak_layer_data + nxy * iz + nx * ix + iy) = (int) (22- *(leak_layer_data + nxy * iz + nx * ix + iy));
-                }
-                if (*(leak_layer_data + nxy * iz + nx * ix + iy) == 20) {
-                    *(leak_layer_data + nxy * iz + nx * ix + iy) = 19;
-                }
-                if (*(leak_layer_data + nxy * iz + nx * ix + iy) == 21) {
-                    *(leak_layer_data + nxy * iz + nx * ix + iy) = 20;
-                }
-            }
+    for (int i = 0; i < nr_voxels; ++i) {
+        if (*(nim_input_data + i) != 0) {
+            *(nii_leaky_data + i) = floor(22 - *(nii_leaky_data + i));
+        }
+        if (*(nii_leaky_data + i) == 20) {
+            *(nii_leaky_data + i) = 19;
+        }
+        if (*(nii_leaky_data + i) == 21) {
+            *(nii_leaky_data + i) = 20;
         }
     }
-    cout << "  Running also until here 5..." << endl;
-
-    // Output file name
-    const char *fout_4 = "leaky_layers.nii";
-    if (nifti_set_filenames(leak_layer, fout_4, 1, 1)) {
-        return 1;
-    }
-    nifti_image_write(leak_layer);
-
-    // const char *fout_5 = "input.nii";
-    // if (nifti_set_filenames(nim_input, fout_5 , 1, 1)) {
-    //     return 1;
-    // }
-    // nifti_image_write(nim_input);
-    //
-    // const char *fout_2 = "gausweights.nii";
-    // if (nifti_set_filenames(gausweight, fout_2 , 1, 1)) {
-    //     return 1;
-    // }
-    // nifti_image_write(gausweight);
-    //
-    // const char *fout_1 = "layer.nii";
-    // if (nifti_set_filenames(layer, fout_1 , 1, 1)) {
-    //     return 1;
-    // }
-    // nifti_image_write(layer);
+    save_output_nifti(fin, "leaky_layers", nii_leaky, true);
 
     cout << "  Finished." << endl;
     return 0;
-}
-
-float dist(float x1, float y1, float z1, float x2, float y2, float z2,
-            float dX, float dY, float dZ) {
-    return sqrt((x1 - x2) * (x1 - x2) * dX * dX
-                + (y1 - y2) * (y1 - y2) * dY * dY
-                + (z1 - z2) * (z1 - z2) * dZ * dZ);
-}
-
-float gaus(float distance, float sigma) {
-    return (1. / (sigma * sqrt(2. * 3.141592))
-            * exp(-0.5 * distance * distance / (sigma * sigma)));
 }
