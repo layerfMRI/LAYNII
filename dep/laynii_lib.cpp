2706c1b6cd90e1586517de71dacf2f0aee555b3a
ofgulban <farukgulban@gmail.com>
2020-03-08 22:32:55 +0100

    Cosmetics

--- dep/laynii_lib.cpp
+++ dep/laynii_lib.cpp
@@ -132,9 +132,8 @@ void log_output(const char* filename) {
 void log_nifti_descriptives(nifti_image* nii) {
     // Print nifti descriptives to command line for debugging
     cout << "  File name: " << nii->fname << endl;
-    cout << "    Image details: " << nii->nz << " Slices | " << nii->nx
-         << " Phase steps | " << nii->ny << " Read steps | " << nii->nt
-         << " Time steps " << endl;
+    cout << "    Image details: " << nii->nz << " X | " << nii->nx
+         << " Y | " << nii->ny << " Z | " << nii->nt << " T " << endl;
     cout << "    Voxel size = " << nii->pixdim[1] << " x " << nii->pixdim[2]
          << " x " << nii->pixdim[3] << endl;
     cout << "    Datatype = " << nii->datatype << "\n" << endl;
