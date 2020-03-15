ff7a10ac40e0c5bd8e5325134333c718f54fce47
ofgulban <farukgulban@gmail.com>
2020-03-15 16:21:17 +0100

    Tidy up, wip...

--- src/LN_3DCOLUMNS.cpp
+++ src/LN_3DCOLUMNS.cpp
@@ -24,7 +24,6 @@ int show_help(void) {
     "\n"
     "Notes:\n"
     "     - Layer nifti and landmarks nifti should have the same dimensions \n"
-    "     - This program now supports INT16, INT32 and FLOAT23 \n"
     "\n");
     return 0;
 }
@@ -67,53 +66,54 @@ int main(int argc, char * argv[]) {
             return 1;
         }
     }
-    if (!fin_landmark) {
-        fprintf(stderr, "** missing option '-landmarks'\n");
-        return 1;
-    }
+
     if (!fin_layer) {
         fprintf(stderr, "** missing option '-layer_file'\n");
         return 1;
     }
+    if (!fin_landmark) {
+        fprintf(stderr, "** missing option '-landmarks'\n");
+        return 1;
+    }
 
     // Read input dataset
-    nifti_image * nii_landmark_r = nifti_image_read(fin_landmark, 1);
-    if (!nii_landmark_r) {
-        fprintf(stderr, "** failed to read NIfTI from '%s'\n", fin_landmark);
+    nifti_image * nii_input1 = nifti_image_read(fin_layer, 1);
+    if (!nii_input1) {
+        fprintf(stderr, "** failed to read NIfTI from '%s'\n", fin_layer);
         return 2;
     }
-    nifti_image * nii_layer_r = nifti_image_read(fin_layer, 1);
-    if (!nii_layer_r) {
-        fprintf(stderr, "** failed to read NIfTI from '%s'\n", fin_layer);
+    nifti_image * nii_input2 = nifti_image_read(fin_landmark, 1);
+    if (!nii_input2) {
+        fprintf(stderr, "** failed to read NIfTI from '%s'\n", fin_landmark);
         return 2;
     }
 
     log_welcome("LN_3DCOLUMNS");
-    log_nifti_descriptives(nii_layer_r);
-    log_nifti_descriptives(nii_landmark_r);
+    log_nifti_descriptives(nii_input1);  // Layer file
+    log_nifti_descriptives(nii_input2);  // Landmarks file
 
     // Get dimensions of input
-    int size_z = nii_layer_r->nz;
-    int size_x = nii_layer_r->nx;
-    int size_y = nii_layer_r->ny;
-    int size_t = nii_layer_r->nt;
-    int nr_voxels = nii_layer_r->nvox;
-    int nx = nii_layer_r->nx;
-    int nxy = nii_layer_r->nx * nii_layer_r->ny;
-    int nxyz = nii_layer_r->nx * nii_layer_r->ny * nii_layer_r->nz;
-    float dX = nii_layer_r->pixdim[1];
-    float dY = nii_layer_r->pixdim[2];
-    float dZ = nii_layer_r->pixdim[3];
+    int size_x = nii_input1->nx;
+    int size_y = nii_input1->ny;
+    int size_z = nii_input1->nz;
+    int size_t = nii_input1->nt;
+    int nr_voxels = nii_input1->nvox;
+    int nx = nii_input1->nx;
+    int nxy = nii_input1->nx * nii_input1->ny;
+    int nxyz = nii_input1->nx * nii_input1->ny * nii_input1->nz;
+    float dX = nii_input1->pixdim[1];
+    float dY = nii_input1->pixdim[2];
+    float dZ = nii_input1->pixdim[3];
     if (twodim == 1) {
         dZ = 1000 * dZ;
     }
 
     // ========================================================================
     // Fixing potential problems with different input datatypes
-    nifti_image* nii_layer = copy_nifti_as_float32(nii_layer_r);
+    nifti_image* nii_layer = copy_nifti_as_float32(nii_input1);
     float* nii_layer_data = static_cast<float*>(nii_layer->data);
 
-    nifti_image* nii_landmark = copy_nifti_as_int32(nii_layer_r);
+    nifti_image* nii_landmark = copy_nifti_as_int32(nii_input1);
     int* nii_landmark_data = static_cast<int*>(nii_landmark->data);
 
     // Allocate necessary files
@@ -150,9 +150,9 @@ int main(int argc, char * argv[]) {
     }
     cout << "  There are " << nr_layers << " layers." << endl;
 
-    ///////////////////////////////
-    // Prepare growing variables //
-    ///////////////////////////////
+    // ========================================================================
+    // Prepare growing variables
+    // ========================================================================
     float x1g = 0., y1g = 0., z1g = 0.;
 
     float dist_min2 = 0.;
@@ -185,35 +185,37 @@ int main(int argc, char * argv[]) {
     }
     for (int grow_i = 1; grow_i < vinc_max; grow_i++) {
         for (int iz = 0; iz < size_z; ++iz) {
-            for (int iy = 0; iy < size_x; ++iy) {
-                for (int ix = 0; ix < size_y; ++ix) {
+            for (int iy = 0; iy < size_y; ++iy) {
+                for (int ix = 0; ix < size_x; ++ix) {
                     dist_min2 = 10000.;
                     x1g = 0;
                     y1g = 0;
                     z1g = 0;
-                    int voxel_i = nxy * iz + nx * ix + iy;
+                    int voxel_i = nxy * iz + nx * iy + ix;
 
                     if (abs((int) (*(nii_layer_data + voxel_i) - nr_layers/2)) < 2
                         && *(growfromCenter_data + voxel_i) == 0
                         && *(nii_landmark_data + voxel_i) < 2) {
-                        // NOTE: Only grow into areas that are GM and that have not been grown into, yet.
-                        // And it should stop as soon as it hits tie border.
+                        // NOTE: Only grow into areas that are GM and that have
+                        // not been grown into, yet. And it should stop as soon
+                        // as it hits tie border.
 
-                        int jz_start = max(0, iy - grow_vinc_area);
-                        int jz_stop = min(iy + grow_vinc_area + 1, size_x);
-                        int jy_start = max(0, ix - grow_vinc_area);
+                        int jz_start = max(0, iz - grow_vinc_area);
+                        int jz_stop = min(iy + grow_vinc_area + 1, size_z);
+                        int jy_start = max(0, iy - grow_vinc_area);
                         int jy_stop = min(ix + grow_vinc_area + 1, size_y);
-                        int jx_start = max(0, iz - grow_vinc_area);
-                        int jx_stop = min(iz + grow_vinc_area + 1, size_z);
+                        int jx_start = max(0, ix - grow_vinc_area);
+                        int jx_stop = min(iz + grow_vinc_area + 1, size_x);
 
                         for (int jz = jz_start; jz < jz_stop; ++jz) {
                             for (int jy = jy_start; jy < jz_stop; ++jy) {
                                 for (int jx = jx_start; jx < jx_stop; ++jx) {
+                                    int voxel_j = nxy * jz + nx * jy + jx;
+
                                     d = dist((float)ix, (float)iy, (float)iz,
                                              (float)jx, (float)jy, (float)jz,
                                              dX, dY, dZ);
 
-                                    int voxel_j = nxy * jz + nx * jy + jx;
                                     if (*(growfromCenter_data + voxel_j) == grow_i
                                         && *(nii_landmark_data + voxel_i) < 2) {
                                         if (d < dist_min2) {
@@ -273,13 +275,13 @@ int main(int argc, char * argv[]) {
     }
     for (int grow_i = 1; grow_i < vinc_max; grow_i++) {
         for (int iz = 0; iz < size_z; ++iz) {
-            for (int iy = 0; iy < size_x; ++iy) {
-                for (int ix = 0; ix < size_y; ++ix) {
+            for (int iy = 0; iy < size_y; ++iy) {
+                for (int ix = 0; ix < size_x; ++ix) {
                     dist_min2 = 10000.;
                     x1g = 0;
                     y1g = 0;
                     z1g = 0;
-                    int voxel_i = nxy * iz + nx * ix + iy;
+                    int voxel_i = nxy * iz + nx * iy + ix;
                     if (abs((int) (*(nii_layer_data + voxel_i) - nr_layers / 2)) < 2
                         && *(growfromLeft_data + voxel_i) == 0
                         && *(growfromCenter_data + voxel_i) != 0) {
@@ -289,9 +291,9 @@ int main(int argc, char * argv[]) {
                         int jz_start = max(0, iz - grow_vinc);
                         int jz_stop = min(iz + grow_vinc + 1, size_z);
                         int jy_start = max(0, iy - grow_vinc);
-                        int jy_stop = min(iy + grow_vinc + 1, size_x);
+                        int jy_stop = min(iy + grow_vinc + 1, size_y);
                         int jx_start = max(0, ix - grow_vinc);
-                        int jx_stop = min(ix + grow_vinc + 1, size_y);
+                        int jx_stop = min(ix + grow_vinc + 1, size_x);
 
                         for (int jy = jy_start; jy < jy_stop; ++jy) {
                             for (int jx = jx_start; jx < jx_stop; ++jx) {
@@ -357,34 +359,35 @@ int main(int argc, char * argv[]) {
     }
     for (int grow_i = 1; grow_i < vinc_max; grow_i++) {
         for (int iz = 0; iz < size_z; ++iz) {
-            for (int iy = 0; iy < size_x; ++iy) {
-                for (int ix = 0; ix < size_y; ++ix) {
+            for (int iy = 0; iy < size_y; ++iy) {
+                for (int ix = 0; ix < size_x; ++ix) {
+                    int voxel_i = nxy * iz + nx * iy + ix;
                     dist_min2 = 10000.;
                     x1g = 0;
                     y1g = 0;
                     z1g = 0;
-                    int voxel_i = nxy * iz + nx * ix + iy;
                     if (abs((int) (*(nii_layer_data + voxel_i) - nr_layers / 2)) < 2
                         && *(growfromRight_data + voxel_i) == 0
                         && *(growfromCenter_data + voxel_i) != 0) {
                         // NOTE: Only grow into areas that are GM and that have not been gown into, yet...
                         // And it should stop as soon as it hits tie border
 
-                        int jy_start = max(0, iy - grow_vinc);
-                        int jy_stop = min(iy + grow_vinc + 1, size_x);
-                        int jx_start = max(0, ix - grow_vinc);
-                        int jx_stop = min(ix + grow_vinc + 1, size_y);
                         int jz_start = max(0, iz - grow_vinc);
                         int jz_stop = min(iz + grow_vinc + 1, size_z);
+                        int jy_start = max(0, iy - grow_vinc);
+                        int jy_stop = min(iy + grow_vinc + 1, size_y);
+                        int jx_start = max(0, ix - grow_vinc);
+                        int jx_stop = min(ix + grow_vinc + 1, size_x);
 
                         for (int jz = jz_start; jz < jz_stop; ++jz) {
                             for (int jy = jy_start; jy < jy_stop; ++jy) {
                                 for (int jx = jx_start; jx < jx_stop; ++jx) {
+                                    int voxel_j = nxy * jz + nx * jy + jx;
+
                                     d = dist((float)ix, (float)iy, (float)iz,
                                              (float)jx, (float)jy, (float)jz,
                                              dX, dY, dZ);
 
-                                    int voxel_j = nxy * jz + nx * jy + jx;
                                     if (*(growfromRight_data + voxel_j) == grow_i) {
                                         if (d < dist_min2) {
                                             dist_min2 = d;
@@ -416,9 +419,9 @@ int main(int argc, char * argv[]) {
     cout << "  Getting normalized coordinate system..." << endl;
 
     for (int iz = 0; iz < size_z; ++iz) {
-        for (int iy = 0; iy < size_x; ++iy) {
-            for (int ix = 0; ix < size_y; ++ix) {
-                int voxel_i = nxy * iz + nx * ix + iy;
+        for (int iy = 0; iy < size_y; ++iy) {
+            for (int ix = 0; ix < size_x; ++ix) {
+                int voxel_i = nxy * iz + nx * iy + ix;
                 if (*(growfromCenter_data + voxel_i) > 0) {
 
                     float grow_r = static_cast<float>(*(growfromRight_data + voxel_i));
@@ -489,7 +492,7 @@ int main(int argc, char * argv[]) {
                     for (int jz = jz_start; jz < jz_stop; ++jz) {
                         for (int jy = jy_start; jy < jy_stop; ++jy) {
                             for (int jx = jx_start; jx < jx_stop; ++jx) {
-                                int voxel_j = nxy * jz + nx * jx + jy;
+                                int voxel_j = nxy * jz + nx * jy + jx;
 
                                 if (*(lateralCoord_data + voxel_j) > 0) {
                                     d = dist((float)ix, (float)iy, (float)iz,
@@ -511,9 +514,8 @@ int main(int argc, char * argv[]) {
     }
     for (int iz = 0; iz < size_z; ++iz) {
         for (int iy = 0; iy < size_y; ++iy) {
-            for (int ix = 0; ix < size_z; ++ix) {
-                int voxel_i = nxy * iz + nx * ix + iy;
-
+            for (int ix = 0; ix < size_x; ++ix) {
+                int voxel_i = nxy * iz + nx * iy + ix;
                 if (*(growfromCenter_data + voxel_i) > 0) {
                     *(lateralCoord_data + voxel_i) =
                         static_cast<int>(*(nii_smooth_data + voxel_i));
@@ -619,13 +621,13 @@ int main(int argc, char * argv[]) {
                     int nr_layers_i = *(nii_layer_data + voxel_i);
                     int jz = iz;
                     int jy_start = max(0, iy - vinc_sm);
-                    int jy_stop = min(iy + vinc_sm + 1, size_x);
+                    int jy_stop = min(iy + vinc_sm + 1, size_y);
                     int jx_start = max(0, ix - vinc_sm);
-                    int jx_stop = min(ix + vinc_sm + 1, size_y);
+                    int jx_stop = min(ix + vinc_sm + 1, size_x);
 
                     for (int jy = jy_start; jz < jy_stop; ++jz) {
                         for (int jx = jx_start; jx < jx_stop; ++jx) {
-                            int voxel_j = voxel_j;
+                            int voxel_j = nxy * jz + nx * jy + jx;
 
                             if (abs((int) *(nii_layer_data + voxel_j) - nr_layers_i) < 2
                                 && *(hairy_data + voxel_j) > 0) {
@@ -711,14 +713,14 @@ int main(int argc, char * argv[]) {
                             && *(hairy_data + voxel_i) > 0) {
 
                             int jz = iz;
-                            int jy_start = max(0, ix - grow_vinc_area_thick);
-                            int jy_stop = min(ix + grow_vinc_area_thick + 1, size_y);
-                            int jx_start = max(0, iy - grow_vinc_area_thick);
-                            int jx_stop = min(iy + grow_vinc_area_thick + 1, size_x);
+                            int jy_start = max(0, iy - grow_vinc_area_thick);
+                            int jy_stop = min(iy + grow_vinc_area_thick + 1, size_y);
+                            int jx_start = max(0, ix - grow_vinc_area_thick);
+                            int jx_stop = min(ix + grow_vinc_area_thick + 1, size_x);
 
                             for (int jy = jy_start; jy < jy_stop; ++jy) {
                                 for (int jx = jx_start; jx < jx_stop; ++jx) {
-                                    int voxel_j = nxy * jz + nx * jx + jy;
+                                    int voxel_j = nxy * jz + nx * jy + jx;
 
                                     d = dist((float)ix, (float)iy, (float)iz,
                                              (float)jx, (float)jy, (float)jz,
@@ -751,10 +753,10 @@ int main(int argc, char * argv[]) {
                             && *(hairy_data + voxel_i) > 0) {
 
                             int jz = iz;
-                            int jy_start = max(0, ix - grow_vinc_area_thick);
-                            int jy_stop = min(ix + grow_vinc_area_thick + 1, size_y);
-                            int jx_start = max(0, iy - grow_vinc_area_thick);
-                            int jx_stop = min(iy + grow_vinc_area_thick + 1, size_x);
+                            int jy_start = max(0, iy - grow_vinc_area_thick);
+                            int jy_stop = min(iy + grow_vinc_area_thick + 1, size_y);
+                            int jx_start = max(0, ix - grow_vinc_area_thick);
+                            int jx_stop = min(ix + grow_vinc_area_thick + 1, size_x);
 
                             for (int jy = jy_start; jy < jy_stop; ++jy) {
                                 for (int jx = jx_start; jx < jx_stop; ++jx) {
@@ -782,15 +784,16 @@ int main(int argc, char * argv[]) {
         for (int iy = 0; iy < size_y; ++iy) {
             for (int ix = 0; ix < size_x; ++ix) {
                 int voxel_i = nxy * iz + nx * iy + ix;
+
                 if ((*(nii_layer_data + voxel_i) == nr_layers - 1
                     || *(nii_layer_data + voxel_i) == nr_layers - 2)
                     && *(growfromCenter_thick_data + voxel_i) > 0) {
 
                     int jz = iz;
                     int jy_start = max(0, iy - grow_vinc_area_thick);
-                    int jy_stop = min(iy + grow_vinc_area_thick + 1, size_x);
+                    int jy_stop = min(iy + grow_vinc_area_thick + 1, size_y);
                     int jx_start = max(0, ix - grow_vinc_area_thick);
-                    int jx_stop = min(ix + grow_vinc_area_thick + 1, size_y);
+                    int jx_stop = min(ix + grow_vinc_area_thick + 1, size_x);
 
                     for (int jy = jy_start; jy < jy_stop; ++jy) {
                         for (int jx = jx_start; jx < jx_stop; ++jx) {
@@ -854,8 +857,8 @@ int main(int argc, char * argv[]) {
     cout << "  Max = " << max_columns << " | Min = " << min_columns << endl;
 
     for (int iz = 0; iz < size_z; ++iz) {
-        for (int iy = 0; iy < size_x; ++iy) {
-            for (int ix = 0; ix < size_y; ++ix) {
+        for (int iy = 0; iy < size_y; ++iy) {
+            for (int ix = 0; ix < size_x; ++ix) {
                 int voxel_i = nxy * iz + nx * iy + ix;
 
                 if (*(hairy_data + voxel_i) >  0) {
