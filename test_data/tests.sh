f3b8d9e23edffb22fa7c45f20716028c38cb17f3
Renzo <layerfMRI@gmail.com>
2020-03-18 14:14:36 +0100

    included gradsmooth test data

--- test_data/tests.sh
+++ test_data/tests.sh
@@ -11,15 +11,15 @@
 ../LN_EXTREMETR -input lo_BOLD_intemp.nii
 ../LN_FLOAT_ME -input sc_rim.nii
 ../LN_GFACTOR -input sc_UNI.nii -output gfactor -variance 1 -direction 1 -grappa 2 -cutoff 5
-../LN_GRADSMOOTH -gradfile lo_T1EPI.nii -input sc_VASO_stat.nii -FWHM 2 -within -selectivity 0.1
+../LN_GRADSMOOTH -gradfile lo_T1EPI.nii -input lo_VASO_stat.nii -FWHM 2 -within -selectivity 0.1
 ../LN_GROW_LAYERS -rim sc_rim.nii
-../LN_INTPRO -image sc_UNI.nii -min -direction 2
+../LN_INTPRO -image sc_UNI.nii -min -direction 2 -range 3
 
 ../LN_LEAKY_LAYERS -rim sc_rim.nii
-../LN_NOISEME -input lo_VASO_stat.nii
+../LN_NOISEME -input lo_VASO_stat.nii -std 1
 ../LN_RAGRUG -input sc_rim.nii
-../LN_SKEW -timeseries lo_BOLD_intemp.nii
+../LN_SKEW -input lo_BOLD_intemp.nii
 ../LN_TEMPSMOOTH -input lo_BOLD_intemp.nii -box 1
 ../LN_TEMPSMOOTH -input lo_BOLD_intemp.nii -gaus 1
-../LN_TRIAL -file lo_BOLD_intemp.nii -trialdur 12
+../LN_TRIAL -input lo_BOLD_intemp.nii -trialdur 20
 ../LN_ZOOM -mask sc_layers_3dcolumns.nii -input sc_UNI.nii
