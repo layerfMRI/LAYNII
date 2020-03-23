f2f49aa22c91b1b9e074813e5d120a931bdaf60f
Renzo <layerfMRI@gmail.com>
2020-03-23 17:01:55 +0100

    included INT_ME

---
+++ src/LN_INT_ME.cpp
@@ -0,0 +1,72 @@
+
+
+#include "../dep/laynii_lib.h"
+
+int show_help(void) {
+    printf(
+    "LN_SHORT_ME: Convert nifti datatype to SHORT (INT16). \n"
+    "\n"
+    "Usage:\n"
+    "    LN_SHORT_ME -input data_file.nii \n"
+    "    LN_SHORT_ME -input data_file.nii -output output_filename.nii \n"
+    "\n"
+    "Options:\n"
+    "    -help   : Show this help.\n"
+    "    -input  : Dataset that should be shorted data.\n"
+    "    -output : (Optional) Output filename.\n"
+    "\n");
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    bool use_outpath = false;
+    char *fin = NULL, *fout = NULL;
+    int ac;
+    if (argc < 3) return show_help();
+
+    // Process user options
+    for (ac = 1; ac < argc; ac++) {
+        if (!strncmp(argv[ac], "-h", 2)) {
+            return show_help();
+        } else if (!strcmp(argv[ac], "-input")) {
+            if (++ac >= argc) {
+                fprintf(stderr, "** missing argument for -input\n");
+                return 1;
+            }
+            fin = argv[ac];
+            fout = fin;
+        } else if (!strcmp(argv[ac], "-output")) {
+            if (++ac >= argc) {
+                fprintf(stderr, "** missing argument for -output\n");
+                return 2;
+            }
+            use_outpath = true;
+            fout = argv[ac];
+        } else {
+            fprintf(stderr, "** invalid option, '%s'\n", argv[ac]);
+            return 1;
+        }
+    }
+
+    if (!fin) {
+        fprintf(stderr, "** missing option '-input'\n");
+        return 1;
+    }
+
+    // Read input dataset
+    nifti_image *nii = nifti_image_read(fin, 1);
+    if (!nii) {
+        fprintf(stderr, "** failed to read NIfTI from '%s'\n", fin);
+        return 2;
+    }
+
+    log_welcome("LN_SHORT_ME");
+    log_nifti_descriptives(nii);
+
+    // Cast input data to short (int16)
+    nifti_image *nii_new = copy_nifti_as_int16(nii);
+    save_output_nifti(fout, "short", nii_new, true, use_outpath);
+
+    cout << "  Finished." << endl;
+    return 0;
+}
