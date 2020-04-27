855f145fd067ddc44fea1ebe09de3aa0f283abf2
layerfMRI <33968961+layerfMRI@users.noreply.github.com>
2020-04-27 12:12:49 +0200

    Update tests.sh

--- test_data/tests.sh
+++ test_data/tests.sh
@@ -13,7 +13,6 @@
 ../LN_DIRECT_SMOOTH -input sc_UNI.nii -FWHM 2 -direction 3
 ../LN_EXTREMETR -input lo_BOLD_intemp.nii
 ../LN_FLOAT_ME -input lo_BOLD_intemp.nii
-../LN_SHORT_ME -input lo_VASO_act.nii -output short.nii
 ../LN_INT_ME -input sc_rim.nii
 ../LN_GFACTOR -input sc_INV2.nii  -variance 1 -direction 1 -grappa 2 -cutoff 200
 ../LN_GRADSMOOTH -gradfile lo_gradT1.nii -input lo_VASO_act.nii -FWHM 1 -within -selectivity 0.1
