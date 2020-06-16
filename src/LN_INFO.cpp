f69f5ff05e0ec6b5457a837909b77bb926e7009e
Renzo <layerfMRI@gmail.com>
2020-06-16 15:52:19 +0200

    Update LN_INFO.cpp

--- src/LN_INFO.cpp
+++ src/LN_INFO.cpp
@@ -73,31 +73,88 @@ int main(int argc, char * argv[]) {
     float* nii_new_data = static_cast<float*>(nii_new->data);
     // ========================================================================
    int sizeSlice = nii_input->nz ; 
-   int sizePhase = nii_input->nx ; 
-   int sizeRead = nii_input->ny ; 
+   int sizePhase = nii_input->ny ; 
+   int sizeRead = nii_input->nx ; 
    int nrep =  nii_input->nt; 
    int nx =  nii_input->nx;
    int nxy = nii_input->nx * nii_input->ny;
    int nxyz = nii_input->nx * nii_input->ny * nii_input->nz;
 
-    // obtain good value range for plotting. 
+    // ========================================================================
+    // signal characteristics. 
+
+
+    cout << endl<< endl<<  "    BRIK value characeristics" << endl;  
+
+    // max value
+    double max_val = -2147483648 ; 
+    int max_x = -1 ; 
+    int max_y = -1 ; 
+    int max_z = -1 ; 
+    int max_t = -1 ; 
     
-    int nr_voxels = nii_input->nvox;
-    for (int i = 0; i < nr_voxels; ++i) {
-        *(nii_new_data + i) += 1 ; 
-    }
-
-    cout << "\n    axial first TR" << endl;
-
-
-for(int iz=0; iz<sizeSlice; ++iz){  
-      for(int iy=0; iy<sizePhase; ++iy){
-        for(int ix=0; ix<sizeRead-0; ++ix){
-          *(nii_new_data    + nxyz *0 + nxy*iz + nx*ix  + iy  ) =  1.  ; 
-
+    for(int it=0; it<nrep; ++it){  
+      for(int iz=0; iz<sizeSlice; ++iz){  
+        for(int iy=0; iy<sizePhase; ++iy){
+          for(int ix=0; ix<sizeRead-0; ++ix){
+            if (*(nii_new_data    + nxyz *it + nxy*iz + nx*iy  + ix  ) > max_val ) {
+                    max_val = *(nii_new_data    + nxyz *it + nxy*iz + nx*iy  + ix  ) ;
+                    max_x = ix ; 
+                    max_y = iy ;
+                    max_z = iz ;
+                    max_t = it ;
+            }  
+          }
         }
       }
     }
+    cout << "    Maximal value is "  << max_val << " at (t="  << max_t<< ",x="<< max_x<< ",y="<< max_y<< ",z="<< max_z<< ")" << endl;  
+
+
+    // min value
+    double min_val = 2147483648 ; 
+    int min_x = -1 ; 
+    int min_y = -1 ; 
+    int min_z = -1 ; 
+    int min_t = -1 ; 
+    
+    for(int it=0; it<nrep; ++it){  
+      for(int iz=0; iz<sizeSlice; ++iz){  
+        for(int iy=0; iy<sizePhase; ++iy){
+          for(int ix=0; ix<sizeRead-0; ++ix){
+            if (*(nii_new_data    + nxyz *it + nxy*iz + nx*iy  + ix  ) < min_val ) {
+                    min_val = *(nii_new_data    + nxyz *it + nxy*iz + nx*iy  + ix  ) ;
+                    min_x = ix ; 
+                    min_y = iy ;
+                    min_z = iz ;
+                    min_t = it ;
+            }  
+          }
+        }
+      }
+    }
+    cout << "    Minimal value is "  << min_val << " at (t="  << min_t<< ",x="<< min_x<< ",y="<< min_y<< ",z="<< min_z<< ")" << endl;  
+
+    // average value
+    double vec1[nxyz*nrep];
+    for (int iv = 0; iv < nxyz*nrep; ++iv) {
+        vec1[iv] =  static_cast<double>(*(nii_new_data + iv)) ; 
+    }
+    cout << "    Average value is "  << ren_average(vec1, nxyz*nrep) << " and STEDV across space and time is " << ren_stdev(vec1, nxyz*nrep)<< endl;  
+
+
+    cout << endl<< endl<<  "    Attempt of plotting in terminal" << endl;  
+ 
+
+    cout << "\n    Axial first TR" << endl;
+
+
+      for(int iy=0; iy<sizePhase; ++iy){
+        for(int ix=0; ix<sizeRead-0; ++ix){
+          *(nii_new_data    + nxyz *0 + nxy*sizeSlice/2 + nx*ix  + iy  )   ; 
+
+        }
+      }
 
    
     cout << "    Finished." << endl;
