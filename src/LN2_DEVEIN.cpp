1d5a3d95eaf920196674f2c5dce6a5f129f95014
ofgulban <farukgulban@gmail.com>
2020-08-02 19:28:35 +0200

    Overhaul flow structure to fix #28 and improve output suffixes - Default is now linear as it requires least amount of inputs. - Program now only checks/loads ALF and columns file if linear mode is deactivated. - This deactivation happens when ALF or columns inputs are given or when CBV flag is on.

--- src/LN2_DEVEIN.cpp
+++ src/LN2_DEVEIN.cpp
@@ -45,9 +45,11 @@ int show_help(void) {
 
 int main(int argc, char* argv[]) {
     char *f_input = NULL, *f_layer = NULL, *f_column = NULL, *f_ALF = NULL;
-    char *f_out = NULL ;
+    char *f_out = NULL;
+    string tag = "devein";
     int ac;
-    bool mode_linear = false, mode_CBV = false;
+    bool mode_linear = true;  // Default is linear as it requires least inputs
+    bool mode_CBV = false;
 
     // Peak to tail ratio from Markuerkiaga et al. 2016 Fig. 5B at 7T.
     float lambda = 0.25;
@@ -68,12 +70,14 @@ int main(int argc, char* argv[]) {
                 fprintf(stderr, "** missing argument for -column_file\n");
                 return 1;
             }
+            mode_linear = false;
             f_column = argv[ac];
         } else if (!strcmp(argv[ac], "-ALF")) {
             if (++ac >= argc) {
                 fprintf(stderr, "** missing argument for -ALF\n");
                 return 1;
             }
+            mode_linear = false;
             f_ALF = argv[ac];
         } else if (!strcmp(argv[ac], "-lambda")) {
             if (++ac >= argc) {
@@ -81,12 +85,9 @@ int main(int argc, char* argv[]) {
                 return 1;
             }
             lambda = atof(argv[ac]);  // No string copy, pointer assignment
-        } else if (!strcmp(argv[ac], "-linear")) {
-            mode_linear = true ;
-            cout << "  There will be no deconvolution, just layer-dependent depth scaling." << endl;
         } else if (!strcmp(argv[ac], "-CBV")) {
-            mode_CBV = true ;
-            cout << "  There will be no deconvolution, just layer-dependent CBV scaling." << endl;
+            mode_linear = false;
+            mode_CBV = true;
         } else if (!strcmp(argv[ac], "-output")) {
             if (++ac >= argc) {
                 fprintf(stderr, "** missing argument for -output\n");
@@ -112,24 +113,12 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    if (mode_linear && mode_CBV) {
-        fprintf(stderr, "  Please either choose Linear or CBV, not both.");
-        return 1;
-    }
     if (!f_input) {
         fprintf(stderr, "** missing option '-input'\n");
         return 1;
     }
     if (!f_layer) {
         fprintf(stderr, "** missing option '-layer_file'\n");
-        return 1;
-    }
-    if (!f_column) {
-        fprintf(stderr, "** missing option '-column_file'\n");
-        return 1;
-    }
-    if (!f_ALF) {
-        fprintf(stderr, "** missing option '-ALF'\n");
         return 1;
     }
 
@@ -144,22 +133,21 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "** failed to read NIfTI from '%s'\n", f_layer);
         return 2;
     }
-    nifti_image* nii_columni = nifti_image_read(f_column, 1);
-    if (!nii_columni) {
-        fprintf(stderr, "** failed to read NIfTI from '%s'\n", f_column);
-        return 2;
-    }
-    nifti_image* nii_ALFi = nifti_image_read(f_ALF, 1);
-    if (!nii_ALFi) {
-        fprintf(stderr, "** failed to read NIfTI from '%s'\n", f_ALF);
-        return 2;
-    }
 
     log_welcome("LN2_DEVEIN");
     log_nifti_descriptives(nii);
     log_nifti_descriptives(nii_layeri);
-    log_nifti_descriptives(nii_columni);
-    log_nifti_descriptives(nii_ALFi);
+
+    if (mode_linear) {
+        tag = "deveinLinear";
+        cout << "  Linear mode is selected!" << endl;
+        cout << "    There will be no deconvolution, just layer-dependent depth scaling.\n" << endl;
+    }
+    if (mode_CBV) {
+        tag = "deveinCBV";
+        cout << "  CBV mode is selected!" << endl;
+        cout << "    There will be no deconvolution, just layer-dependent CBV scaling.\n" << endl;
+    }
 
     // Get dimensions of input
     const int size_x = nii->nx;
@@ -174,16 +162,12 @@ int main(int argc, char* argv[]) {
     float *nii_input_data = static_cast<float*>(nii_input->data);
     nifti_image* nii_layer = copy_nifti_as_int32(nii_layeri);
     int32_t *nii_layer_data = static_cast<int32_t*>(nii_layer->data);
-    nifti_image* nii_column = copy_nifti_as_int32(nii_columni);
-    int32_t *nii_column_data = static_cast<int32_t*>(nii_column->data);
-    nifti_image* nii_ALF = copy_nifti_as_float32(nii_ALFi);
-    float *nii_ALF_data = static_cast<float*>(nii_ALF->data);
 
     // Allocate new niftis
-    nifti_image *nii_decov = copy_nifti_as_float32(nii_input);
-    float *nii_decov_data = static_cast<float*>(nii_decov->data);
+    nifti_image *nii_output = copy_nifti_as_float32(nii_input);
+    float *nii_output_data = static_cast<float*>(nii_output->data);
     for (int i = 0; i < nr_voxels * size_t; ++i) {
-        *(nii_decov_data + i) = 0;
+        *(nii_output_data + i) = 0;
     }
 
     // ------------------------------------------------------------------------
@@ -197,52 +181,84 @@ int main(int argc, char* argv[]) {
     }
     cout << "  Number of layers = " << nr_layers << endl;
 
-    // ------------------------------------------------------------------------
-    // Find number of columns
-    // ------------------------------------------------------------------------
-    int nr_columns = 0;
-    for (int i = 0; i < nr_voxels; ++i) {
-        if (*(nii_column_data + i) > nr_columns) {
-            nr_columns = *(nii_column_data + i);
+    // ========================================================================
+    // Main chunk
+    // ========================================================================
+    if (mode_linear) {  // Handle linear case straightforwardly
+        for (int t=0; t<size_t; ++t) {
+            for (int i=0; i<nr_voxels; ++i) {
+                int j = t * nr_voxels + i;
+                if (*(nii_layer_data + i) > 0) {
+                    float l = *(nii_layer_data + i);
+                    *(nii_output_data + j) = *(nii_input_data + j) / l;
+                    *(nii_output_data + j) *= (float)nr_layers;
+                }
+            }
         }
-    }
-    cout << "  Number of columns = " << nr_columns << endl;
+    } else {
 
-    // ========================================================================
-    // Do deconvolution column by column. First, I allocate all.
-    // ========================================================================
-    float vec1[nr_layers][size_t], vec2[nr_layers][size_t], vecALF[nr_layers];
-    int vec_nr_voxels[nr_layers];
-
-    for (int i = 0; i < nr_layers; ++i) {
-        for (int t = 0; t < size_t; t++) {
-            vec1[i][t] = 0.;
-            vec2[i][t] = 0.;
+        // Check additional inputs
+        if (!f_column) {
+            fprintf(stderr, "** missing option '-column_file'\n");
+            return 1;
         }
-        vecALF[i] = 0.;
-        vec_nr_voxels[i] = 0;
-    }
-
-    // ------------------------------------------------------------------------
-    // Making sure that every column voxel has a layer
-    // ------------------------------------------------------------------------
-    for (int ivox = 0; ivox < nr_voxels; ++ivox) {
-        if (*(nii_column_data + ivox) > 0 &&  *(nii_layer_data + ivox) == 0 ){
-            cout << "  Some column voxels don't have layers in this file.\n";
-            cout << "  It is likely that you provided wrong data.\n";
-            cout << "  Though, the program will proceed." << endl;
-            *(nii_column_data + ivox) = 0;
+        if (!f_ALF) {
+            fprintf(stderr, "** missing option '-ALF'\n");
+            return 1;
         }
-    }
 
-    // ========================================================================
-    // Big loop across columns
-    // ========================================================================
-    for (int icol = 1; icol <= nr_columns; ++icol) {
+        // Read additional inputs
+        nifti_image* nii_columni = nifti_image_read(f_column, 1);
+        if (!nii_columni) {
+            fprintf(stderr, "** failed to read NIfTI from '%s'\n", f_column);
+            return 2;
+        }
+        nifti_image* nii_ALFi = nifti_image_read(f_ALF, 1);
+        if (!nii_ALFi) {
+            fprintf(stderr, "** failed to read NIfTI from '%s'\n", f_ALF);
+            return 2;
+        }
 
-        // Reset vector
+        log_nifti_descriptives(nii_columni);
+        log_nifti_descriptives(nii_ALFi);
+
+        // Prepare additional inputs
+        nifti_image* nii_column = copy_nifti_as_int32(nii_columni);
+        int32_t *nii_column_data = static_cast<int32_t*>(nii_column->data);
+        nifti_image* nii_ALF = copy_nifti_as_float32(nii_ALFi);
+        float* nii_ALF_data = static_cast<float*>(nii_ALF->data);
+
+        // --------------------------------------------------------------------
+        // Find number of columns
+        // --------------------------------------------------------------------
+        int nr_columns = 0;
+        for (int i = 0; i < nr_voxels; ++i) {
+            if (*(nii_column_data + i) > nr_columns) {
+                nr_columns = *(nii_column_data + i);
+            }
+        }
+        cout << "  Number of columns = " << nr_columns << endl;
+
+        // --------------------------------------------------------------------
+        // Making sure that every column voxel has a layer
+        // --------------------------------------------------------------------
+        for (int ivox = 0; ivox < nr_voxels; ++ivox) {
+            if (*(nii_column_data + ivox) > 0 &&  *(nii_layer_data + ivox) == 0 ){
+                cout << "  Some column voxels don't have layers in this file.\n";
+                cout << "  It is likely that you provided wrong data.\n";
+                cout << "  Though, the program will proceed." << endl;
+                *(nii_column_data + ivox) = 0;
+            }
+        }
+
+        // ====================================================================
+        // Do deconvolution column by column. First, I allocate all.
+        // ====================================================================
+        float vec1[nr_layers][size_t], vec2[nr_layers][size_t], vecALF[nr_layers];
+        int vec_nr_voxels[nr_layers];
+
         for (int i = 0; i < nr_layers; ++i) {
-            for (int t = 0; t < size_t; t++){
+            for (int t = 0; t < size_t; t++) {
                 vec1[i][t] = 0.;
                 vec2[i][t] = 0.;
             }
@@ -250,87 +266,102 @@ int main(int argc, char* argv[]) {
             vec_nr_voxels[i] = 0;
         }
 
-        // Fill vector of column #icol
-        for (int ivox = 0; ivox < nr_voxels; ++ivox) {
-            if (icol == *(nii_column_data + ivox)) {
-                int i = *(nii_layer_data + ivox) - 1;  // current layer
-                vecALF[i] += *(nii_ALF_data + ivox);
-                vec_nr_voxels[i] += 1;
-
-                for (int t = 0; t < size_t; t++) {
-                    vec1[i][t] += *(nii_input_data + t * nr_voxels + ivox);
-                }
-            }
-        }
-
-        // Get mean of values within column vector
-        for (int i = 0; i < nr_layers; ++i) {
-            for (int t = 0; t < size_t; t++) {
-                vec1[i][t] /= (float)vec_nr_voxels[i];
-            }
-            vecALF[i] /= (float)vec_nr_voxels[i];
-        }
-
         // ====================================================================
-        // Do voxel deconvolution
+        // Big loop across columns
         // ====================================================================
+        for (int icol = 1; icol <= nr_columns; ++icol) {
 
-        // Normalize amplitude of low frequencies (ALF)
-        float ALF_sum = 0 ;
-        for (int i = 0; i < nr_layers; ++i) {
-            if (vec_nr_voxels[i] > 0) {
-                ALF_sum += vecALF[i];
-            }
-        }
-        for (int i = 0; i < nr_layers; ++i) {
-            if (vec_nr_voxels[i] > 0) {
-                vecALF[i] /= ALF_sum;
-            }
-        }
-
-        for (int t = 0; t < size_t; t++){
-
-            // Get weighted sum
+            // Reset vector
             for (int i = 0; i < nr_layers; ++i) {
-                // Macrovascular contribution value, that needs to be
-                // subtracted from the current voxel.
-                float sum = 0;
-                for (int j = i-1; j >= 0; --j) {
-                    // This is the deconvolution, It is weighted with CBV.
-                    // Lambda is the inverse of peak to tail ratio
-                    // from from Markuerkiaga et al. 2016 Fig. 5B at 7T.
-                    if (vec_nr_voxels[j] > 0) {
-                        sum += vec1[j][t] / (float)nr_layers / vecALF[j] * lambda;
+                for (int t = 0; t < size_t; t++){
+                    vec1[i][t] = 0.;
+                    vec2[i][t] = 0.;
+                }
+                vecALF[i] = 0.;
+                vec_nr_voxels[i] = 0;
+            }
+
+            // Fill vector of column #icol
+            for (int ivox = 0; ivox < nr_voxels; ++ivox) {
+                if (icol == *(nii_column_data + ivox)) {
+                    int i = *(nii_layer_data + ivox) - 1;  // current layer
+
+                    if (!mode_linear) {
+                        vecALF[i] += *(nii_ALF_data + ivox);
+                    }
+
+                    vec_nr_voxels[i] += 1;
+
+                    for (int t = 0; t < size_t; t++) {
+                        vec1[i][t] += *(nii_input_data + t * nr_voxels + ivox);
                     }
                 }
+            }
+
+            // Get mean of values within column vector
+            for (int i = 0; i < nr_layers; ++i) {
+                for (int t = 0; t < size_t; t++) {
+                    vec1[i][t] /= (float)vec_nr_voxels[i];
+                }
+                vecALF[i] /= (float)vec_nr_voxels[i];
+            }
+
+            // ----------------------------------------------------------------
+            // Do voxel deconvolution
+            // ----------------------------------------------------------------
+
+            // Normalize amplitude of low frequencies (ALF)
+            float ALF_sum = 0 ;
+            for (int i = 0; i < nr_layers; ++i) {
                 if (vec_nr_voxels[i] > 0) {
-                    vec2[i][t] = (vec1[i][t] - sum);
-                }
-
-                // Just linear correction
-                if (vec_nr_voxels[i] > 0 && mode_linear) {
-                    vec2[i][t] = vec1[i][t] / (float)(i+1) * nr_layers;
-                }
-
-                // Just CBV normalization
-                if (vec_nr_voxels[i] > 0 && mode_CBV) {
-                    vec2[i][t] = vec1[i][t] / vecALF[i] * (float)nr_layers;
+                    ALF_sum += vecALF[i];
                 }
             }
-        }
+            for (int i = 0; i < nr_layers; ++i) {
+                if (vec_nr_voxels[i] > 0) {
+                    vecALF[i] /= ALF_sum;
+                }
+            }
 
-        // Fill file with the deconvolved values
-        for (int ivox = 0; ivox < nr_voxels; ++ivox) {
-            if (icol == *(nii_column_data + ivox)) {
-                int i = *(nii_layer_data + ivox) - 1;
-                for (int t = 0; t < size_t; t++) {
-                    *(nii_decov_data + t * nr_voxels + ivox ) = vec2[i][t];
+            for (int t = 0; t < size_t; t++){
+
+                for (int i = 0; i < nr_layers; ++i) {
+                    if (mode_CBV) {  // Just CBV normalization
+                        if (vec_nr_voxels[i] > 0) {
+                            vec2[i][t] = vec1[i][t] / vecALF[i] * (float)nr_layers;
+                        }
+                    } else {  // Deconvolution
+                        // Macrovascular contribution value, that needs to be
+                        // subtracted from the current voxel.
+                        float sum = 0;
+                        for (int j = i-1; j >= 0; --j) {
+                            // This is the deconvolution, It is weighted with CBV.
+                            // Lambda is the inverse of peak to tail ratio
+                            // from from Markuerkiaga et al. 2016 Fig. 5B at 7T.
+                            if (vec_nr_voxels[j] > 0) {
+                                sum += vec1[j][t] / (float)nr_layers / vecALF[j] * lambda;
+                            }
+                        }
+                        if (vec_nr_voxels[i] > 0) {
+                            vec2[i][t] = (vec1[i][t] - sum);
+                        }
+                    }
+                }
+            }
+
+            // Fill file with the deconvolved values
+            for (int ivox = 0; ivox < nr_voxels; ++ivox) {
+                if (icol == *(nii_column_data + ivox)) {
+                    int i = *(nii_layer_data + ivox) - 1;
+                    for (int t = 0; t < size_t; t++) {
+                        *(nii_output_data + t * nr_voxels + ivox ) = vec2[i][t];
+                    }
                 }
             }
         }
     }
 
-    save_output_nifti(f_out, "deconvolved", nii_decov, true);
+    save_output_nifti(f_out, tag, nii_output, true);
 
     cout << "  Finished." << endl;
     return 0;
