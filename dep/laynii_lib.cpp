59253471b11849bedf2f65960a88c7dac5594292
Remi Gau <remi_gau@hotmail.com>
2022-03-29 19:07:42 +0200

    update version

--- dep/laynii_lib.cpp
+++ dep/laynii_lib.cpp
@@ -7,7 +7,7 @@
 
 void log_welcome(const char* programname) {
     cout << "======================="<< endl;
-    cout << "LAYNII v2.2.0          "<< endl;
+    cout << "LAYNII v2.2.1dev          "<< endl;
 //    cout << "Compiled for Mac"<< endl;
     cout << "======================="<< endl;
     cout << programname << "\n" << endl;
