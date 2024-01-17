0138f9cb280afa51f5283e875efe358d831db0ca
ofgulban <farukgulban@gmail..com>
2024-01-17 16:31:43 +0100

    Version bump

--- dep/laynii_lib.cpp
+++ dep/laynii_lib.cpp
@@ -7,7 +7,7 @@
 
 void log_welcome(const char* programname) {
     cout << "======================="<< endl;
-    cout << "LayNii v2.5.3          "<< endl;
+    cout << "LayNii v2.6.0          "<< endl;
     cout << "======================="<< endl;
     cout << programname << "\n" << endl;
 }
