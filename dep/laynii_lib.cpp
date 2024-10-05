23b47c16f5b46e7a09e98713c5e9836c58676b17
Omer Faruk Gulban <farukgulban@gmail.com>
2024-10-05 17:52:00 +0200

    Version bump

--- dep/laynii_lib.cpp
+++ dep/laynii_lib.cpp
@@ -7,7 +7,7 @@
 
 void log_welcome(const char* programname) {
     cout << "======================="<< endl;
-    cout << "LayNii v2.7.0          "<< endl;
+    cout << "LayNii v2.7.1          "<< endl;
     cout << "======================="<< endl;
     cout << programname << "\n" << endl;
 }
